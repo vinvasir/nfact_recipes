acdc32cbf11d7edd308cccca92f3c44efab89b4f
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-11-01 16:01:51 -0400

    auth is now set up

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from rest_framework.authtoken import views as tokenauth_views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
-  url(r'^token-auth/$', tokenauth_views.obtain_auth_token),
+  url(r'^tokenauth/$', tokenauth_views.obtain_auth_token),
   url(r'^allrecipes/(?P<slug>[\w-]+)/$', views.allrecipes),
 ]
