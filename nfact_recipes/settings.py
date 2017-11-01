9419db9fc85077e481b967fbd24a0c585107ae4a
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-11-01 13:14:14 -0400

    config for heroku

--- nfact_recipes/settings.py
+++ nfact_recipes/settings.py
@@ -83,6 +83,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -121,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
