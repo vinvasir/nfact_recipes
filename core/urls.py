9f985182c7402f1b146dd7467de50fbbba4da167
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-11-01 17:23:36 -0400

    get rid of the tokenauth url

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,5 @@ from rest_framework.authtoken import views as tokenauth_views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
-  url(r'^tokenauth/$', tokenauth_views.obtain_auth_token),
   url(r'^allrecipes/(?P<slug>[\w-]+)/$', views.allrecipes),
 ]
