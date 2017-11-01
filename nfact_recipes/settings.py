250a0314c898cc6fdc0bdcce06cc24d73a2b3013
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-11-01 13:19:15 -0400

    static root?

--- nfact_recipes/settings.py
+++ nfact_recipes/settings.py
@@ -124,4 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/public/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
